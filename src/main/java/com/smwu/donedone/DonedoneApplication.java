ac9debbd8c316a36751db89c93357e0d7c0a66f1
Juhyung990122 <harukawa99@naver.com>
2023-01-20 01:53:26 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/smwu/donedone/DonedoneApplication.java
@@ -0,0 +1,13 @@
+package com.smwu.donedone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonedoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonedoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smwu/donedone/DonedoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smwu.donedone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonedoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
