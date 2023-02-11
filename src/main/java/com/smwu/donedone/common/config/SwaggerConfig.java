69552ce4ddd3ded81c2c89b00c6825911e7e3277
Juhyung990122 <harukawa99@naver.com>
2023-02-11 16:00:58 +0900

    docs: 문서화 적용

---
+++ src/main/java/com/smwu/donedone/common/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.smwu.donedone.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "던던 API",
+                description = "던던 API 문서입니다"
+        ))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/**"};
+
+        return GroupedOpenApi.builder()
+                .group("던던 API")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
